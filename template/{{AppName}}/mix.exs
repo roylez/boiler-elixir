1c8e98bd5749320b79442161f369f3a5520371ce
roylez <me@roylez.info>
2017-03-15 09:01:45 +1000

    add default module for application start

--- template/{{AppName}}/mix.exs
+++ template/{{AppName}}/mix.exs
@@ -19,7 +19,10 @@ defmodule {{ ModuleName }}.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [ 
+      extra_applications: [:logger],
+      mod: { {{ ModuleName }}, [ ] }
+    ]
   end
 
   # Dependencies can be Hex packages:
